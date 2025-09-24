e1db8346afe39909b32ab6547e6bbb9401432777
CookedSeafood <125453632+CookedSeafood@users.noreply.github.com>
2025-09-24 15:55:52 +0800

    Update to 0.2.0.

--- src/main/java/net/cookedseafood/generalcustomdata/GeneralCustomData.java
+++ src/main/java/net/cookedseafood/generalcustomdata/GeneralCustomData.java
@@ -15,8 +15,8 @@ public class GeneralCustomData implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final byte VERSION_MAJOR = 0;
-    public static final byte VERSION_MINOR = 1;
-    public static final byte VERSION_PATCH = 6;
+    public static final byte VERSION_MINOR = 2;
+    public static final byte VERSION_PATCH = 0;
 
     public static final String MOD_NAMESPACE = "general_custom_data";
 
